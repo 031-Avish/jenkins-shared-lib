e03cd5ba8b6ca726c0230b8c7b97a001ae5de64e
avish <amehta@presidio.com>
2024-12-13 15:21:08 +0530

    initial commit

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,11 @@
 def call(){
-  sh 'echo hello world' 
+  sh 'echo using jenkins shared library'
+  // build docker image
+
+  sh "docker build -t ${imageName}:${tag} ."
+
+  // tag the docker umage 
+  sh "docker tag ${imageName}:${tag} ${ecrRepository}:${tag}"
+
+  sh "docker push ${ecrRepository}:${tag}"
 }
